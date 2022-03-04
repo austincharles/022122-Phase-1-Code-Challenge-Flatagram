a447a8bbdbc6c7d2898332ed730a6db98bab7d78
Austin Holeman <austinholeman@gmail.com>
2022-03-04 10:33:47 -0800

    final code challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-console.log('hagsjhwbhckj')
+const URL = 'http://localhost:3000/images/1'
+const imageContainer = document.getElementsByClassName('image-container')
+const likesSection = document.getElementsByClassName('likes-section')
+const commentsList = document.getElementById('comments-list')
+const likesButton = document.getElementById('like-button')
+const commentForm = document.getElementById('comment-form')
+
+fetch(URL)
+    .then(resp => resp.json())
+    .then(renderDogPost)
+
+likesButton.addEventListener('click', function (e) {
+    ++dog.likes;
+    likes.textContent = dog.likes
+})
+
+commentForm.addEventListener('submit', addNewComment())
+
+function renderDogPost(dogArray) {
+    dogArray.forEach(addPostToPage())
+}
+
+function addPostToPage(dog) {
+    const dogImage = document.createElement('img')
+    dogImage.src = dog.images;
+    imageContainer.append(dogImage)
+}
+
+function displayPostDetails(dog) {
+    const cardImage = document.querySelector('.card-image')
+    cardImage.src = dog.image;
+    cardImage.alt = dog.title;
+
+    document.querySelector('.title').textContent = dog.title;
+    document.querySelector('.likes').textContent = dog.likes
+    document.querySelector('.comments').textContent = dog.comments
+
+    likesSection.textContent = dog.likes
+    commentsList.textContent = dog.comments
+}
+
+function addNewComment(event) {
+    event.preventDefault()
+    console.log(event.target.comment.value)
+
+    const newComment = {
+        comment: event.target['new-comment'].value,
+    }
+}
